71b954b677f8514d025a82667f48b52c148d1d50
Gartic99 <artic.1804049@studenti.uniroma1.it>
2022-05-03 21:38:52 +0200

    server e docker che parte

--- code/Docker/main.py
+++ code/Docker/main.py
@@ -1,46 +1,44 @@
+#!/usr/bin/env python
+from flask import Flask, render_template, Response
 import cv2
+import datetime
+import time
 import matplotlib.pyplot as plt
 import cvlib as cv
 import urllib.request
 import numpy as np
+import argparse
 from cvlib.object_detection import draw_bbox
-import concurrent.futures
- 
-url='http://192.168.86.25/cam-hi.jpg'
+
+global url
 im=None
- 
-def run1():
-    cv2.namedWindow("live transmission", cv2.WINDOW_AUTOSIZE)
-    while True:
-        img_resp=urllib.request.urlopen(url)
-        imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
-        im = cv2.imdecode(imgnp,-1)
- 
-        cv2.imshow('live transmission',im)
-        key=cv2.waitKey(5)
-        if key==ord('q'):
-            break
-            
-    cv2.destroyAllWindows()
-        
+
+app = Flask(__name__)
+
+
 def run2():
-    cv2.namedWindow("detection", cv2.WINDOW_AUTOSIZE)
     while True:
         img_resp=urllib.request.urlopen(url)
         imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
         im = cv2.imdecode(imgnp,-1)
         bbox, label, conf = cv.detect_common_objects(im)
+        ct = datetime.datetime.now()
+        print(ct,": ",bbox, label, conf)
         im = draw_bbox(im, bbox, label, conf)
- 
-        cv2.imshow('detection',im)
-        key=cv2.waitKey(5)
-        if key==ord('q'):
-            break
-            
-    cv2.destroyAllWindows()
- 
- 
- 
+        frame = cv2.imencode('.jpg', im)[1].tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(run2(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+    
 if __name__ == '__main__':
-    print("started")
-    run2()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "url", type=str, help="File containing data you want to evaluate upon"
+    )
+    args = parser.parse_args()
+    url = 'http://'+args.url+'/cam-hi.jpg'
+    print(url)
+    app.run(host='0.0.0.0',port=12345)#, debug=True)
--- code/Docker/script.sh
+++ code/Docker/script.sh
@@ -9,14 +9,16 @@ docker ps -aq --filter "name=astli" | grep -q . && docker rm astli
 # build docker file
 docker build . -f Dockerfile -t astli
 
-# bring model up
-docker run -d -p 12345:12345 --name astli astli
+#get cam ip
+s=`arp -a | grep 30:c6:f7:20:61:e8 | awk '{while ( match($0,/([0-9]+\.){3}[0-9]+/) ) { print substr($0,RSTART,RLENGTH); $0=substr($0,RSTART+RLENGTH) } }'`
 
+# bring model up
+docker run -p 12345:12345 --name astli astli "$s"
 # perform evaluation
-/usr/bin/env python main.py
+#/usr/bin/env python main.py
 
 # stop container
-docker stop astli
+#docker stop astli
 
 # dump container logs
 docker logs -t astli > logs/server.stdout 2> logs/server.stderr
