5f6a255eb3ce8c302ae685367efd7d80111facb9
Gartic99 <artic.1804049@studenti.uniroma1.it>
2022-05-06 01:26:16 +0200

    allineamento script

--- code/Docker/script.sh
+++ code/Docker/script.sh
@@ -10,8 +10,13 @@ docker ps -aq --filter "name=astli" | grep -q . && docker rm astli
 docker build . -f Dockerfile -t astli
 
 #get cam ip
-s=`arp -a | grep 30:c6:f7:20:61:e8 | awk '{while ( match($0,/([0-9]+\.){3}[0-9]+/) ) { print substr($0,RSTART,RLENGTH); $0=substr($0,RSTART+RLENGTH) } }'`
 
+echo "searching for cam"  
+while [ -z "$s" ] 
+do 
+s=`arp -a | grep 30:c6:f7:20:61:e8 | awk '{while ( match($0,/([0-9]+\.){3}[0-9]+/) ) { print substr($0,RSTART,RLENGTH); $0=substr($0,RSTART+RLENGTH) } }'`
+done
+echo "cam found"
 # bring model up
 docker run -p 12345:12345 --name astli astli "$s"
 
@@ -22,4 +27,4 @@ docker stop astli
 docker logs -t astli > logs/server.stdout 2> logs/server.stderr
 
 # remove container
-docker rm astli
+#docker rm astli
